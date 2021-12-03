c5f602bc734bbf639fa8ad037a58bd72974f5fa1
Patrick Sullivan <psullivang7@gmail.com>
2021-12-02 19:58:17 -0500

    Clean up solution with filter

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -6,44 +6,23 @@ fn main() {
         .expect("Unable to read from file");
 
     println!("{}", part_1(&input));
-
     println!("{}", part_2(&input));
 }
 
-fn part_1(input: &String) -> u32 {
-    let mut previous_depth: Option<u32> = None;
+fn part_1(input: &String) -> usize {
     input
         .lines()
-        .fold(0, |mut acc: u32, line| {
-            let current_depth: u32 = line.parse().unwrap_or(0);
-
-            if let Some(prev_depth) = previous_depth {
-                if current_depth > prev_depth {
-                    acc += 1;
-                }
-            }
-            previous_depth = Some(current_depth);
-            acc
-        })
+        .map(|x| x.parse::<u32>().unwrap_or(0))
+        .tuple_windows()
+        .filter(|(x, y)| x < y)
+        .count()
 }
 
-fn part_2(input: &String) -> u32 {
-    let mut previous_depth: Option<u32> = None;
+fn part_2(input: &String) -> usize {
     input
         .lines()
+        .map(|x| x.parse::<u32>().unwrap_or(0))
         .tuple_windows()
-        .fold(0, |mut acc, (a, b, c)| {
-            let a: u32 = a.parse().unwrap_or(0);
-            let b: u32 = b.parse().unwrap_or(0);
-            let c: u32 = c.parse().unwrap_or(0);
-            let sum = a + b + c;
-            
-            if let Some(prev_depth) = previous_depth {
-                if sum > prev_depth {
-                    acc += 1;
-                }
-            }
-            previous_depth = Some(sum);
-            acc
-        })
+        .filter(|(x, y, z, w)| x + y + z < y + z + w)
+        .count()
 }
